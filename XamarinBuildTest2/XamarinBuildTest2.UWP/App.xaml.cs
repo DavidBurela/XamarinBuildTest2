63effb9bdecd3a95cdf184e58a0eeb047dd3d83b
David Burela <David.Burela@gmail.com>
2016-07-29 16:17:24 -0700

    triggering build

--- XamarinBuildTest2/XamarinBuildTest2.UWP/App.xaml.cs
+++ XamarinBuildTest2/XamarinBuildTest2.UWP/App.xaml.cs
@@ -16,7 +16,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 namespace XamarinBuildTest2.UWP
-{
+{ 
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
